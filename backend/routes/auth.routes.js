ab6e89e1f5a26efb8c37b4593c2aa78bf0a09b3d
Ashik Rahman <tntashikrahman@gmail.com>
2025-03-18 06:39:40 +0600

    First part completed- Backend

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,12 @@
+// auth.routes.js
+import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router;
