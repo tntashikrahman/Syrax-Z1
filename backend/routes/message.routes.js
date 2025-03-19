271375601b186fce5d8178da6dd1b21dba4782a5
Ashik Rahman <tntashikrahman@gmail.com>
2025-03-19 12:11:51 +0600

    Second part completed - Frontend

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { sendMessage, getMessages } from "../controllers/message.controller.js";
+import { getMessages, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
+
 const router = express.Router();
 
 router.get("/:id", protectRoute, getMessages);
